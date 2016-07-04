3160767168cd15dab51f3534ea2e4b579a488f91
ilebedyuk <ilebeduyk@gmail.com>
2016-07-04 17:50:54 +0300

    Add counter for rows on UI

--- src/main/resources/META-INF/resources/WEB-INF/jsp/static/races.js
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/static/races.js
@@ -8,46 +8,54 @@ $(document).ready(function() {
         });
 });
 
-$(document).ready(function() {
-    $('#property-overview').dataTable({
-        //"sPaginationType": "full_numbers",
-        columns: [
-            { "data":"id"},
-            { "data":"name"},
-            { "data":"rank"}
-        ],
-        "bProcessing": true,
-        "sAjaxDataProp":"",
-        bServerSide: true,
-        sAjaxSource: "/humans",
-        sServerMethod: "POST",
-        "bDestroy": true
-    });
-});
+
+//$(document).ready(function() {
+//    $('#property-overview').dataTable({
+//        //"sPaginationType": "full_numbers",
+//
+//        columns: [
+//            { "data": count++},
+//            { "data":"name"},
+//            { "data":"rank"}
+//        ],
+//        "bProcessing": true,
+//        "sAjaxDataProp":"",
+//        bServerSide: true,
+//        sAjaxSource: "/humans",
+//        sServerMethod: "POST",
+//        "bDestroy": true
+//    });
+//});
 
 
 $(document).on('change', '#raceSelect', function() {
     console.log($(this).val()); // the selected options’s value
+
     if ($(this).val() === 'ORKS') {
-        $('#property-overview').dataTable({
+        var image_table = $('#property-overview').dataTable({
             //"sPaginationType": "full_numbers",
             columns: [
-                { "data":"id"},
+                { "data": null},
                 { "data":"name"},
                 { "data":"rank"}
             ],
             "bProcessing": true,
+            "fnDrawCallback": function() {
+                recalculate_image_row_numbers();
+            },
             "sAjaxDataProp":"",
             bServerSide: true,
             sAjaxSource: "/orks",
             sServerMethod: "POST",
             "bDestroy": true
         });
+
+
     } else {
-        $('#property-overview').dataTable({
+        var image_table = $('#property-overview').dataTable({
             //"sPaginationType": "full_numbers",
             columns: [
-                { "data":"id"},
+                { "data": null},
                 { "data":"name"},
                 { "data":"rank"}
             ],
@@ -56,8 +64,20 @@ $(document).on('change', '#raceSelect', function() {
             bServerSide: true,
             sAjaxSource: "/humans",
             sServerMethod: "POST",
-            "bDestroy": true
+            "bDestroy": true,
+            "fnDrawCallback": function() {
+                recalculate_image_row_numbers();
+            },
         });
+    }
+
+    function recalculate_image_row_numbers() {
+        if (typeof image_table != "undefined") {
+            var table_rows = image_table.fnGetNodes();
+            $.each(table_rows, function(index){
+                $("td:first", this).html(index+1);
+            });
+        }
     }
 });
 
